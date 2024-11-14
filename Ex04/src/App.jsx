bb54d73870432dab39dd89495e43d0e40f3e6330
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-11-14 16:44:20 -0300

    Finish

--- Ex04/src/App.jsx
+++ Ex04/src/App.jsx
@@ -6,19 +6,23 @@ function App() {
 
   return (
     <>
+    {/* Criando uma NavBar  */}
       <div className='NavBar'>
       </div>
-
+{/* Criando sections para melhor organizalçao */}
       <section id='section01'>
         <div className='foto'>
-          <img src="https://image.api.playstation.com/cdn/UP0151/CUSA09971_00/FEs8B2BDAudxV3js6SM2t4vZ88vnxSi0.png?w=440&thumb=false" alt="" width={400} height={400} />
+          {/* colocando a imagem de perfil */}
+          <img src="https://image.api.playstation.com/cdn/UP0151/CUSA09971_00/FEs8B2BDAudxV3js6SM2t4vZ88vnxSi0.png?w=440&thumb=false" alt="Rick do rick and morty" width={400} height={400} />
         </div>
+        {/* Informações do usuario, apresentação alinhados a direita */}
         <div className='nomeDireita'>
           <h1>Nome do usuario</h1>
           <p>Sua profissão - função</p>
         </div>
       </section>
       <section id='section02'>
+        {/* utilização da classe conteiner para deixar mais organizado = classe pai e as outras filhas  */}
         <div className="container">
             <div className='esquerda'>
               <h1>
